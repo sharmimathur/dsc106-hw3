cea4b2c7d7d949d64cde12bec0b9586b140c0d23
Sharmi Mathur <s3mathur@ucsd.edu>
2019-11-13 15:09:39 -0800

    deleted demand and more progress

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 'use strict';
 
 const JSONFileName = 'assets/sample_data.json';
+var globalEnergyData = {};
+
+
 
 ['mousemove', 'touchmove', 'touchstart'].forEach(function (eventType) {
     document.getElementById('container').addEventListener(
@@ -19,9 +22,28 @@ const JSONFileName = 'assets/sample_data.json';
                 // Get the hovered point
                 point = chart.series[0].searchPoint(event, true);
 
+                //OPTIONS
+                //could get x, access the dataset, and use that for the series
+                // access through tooltips?
+                //
+
                 if (point) {
                     point.highlight(e);
                 }
+
+                console.log('index: ' + point.index);
+                
+                if (i == 1) {
+                    var pieData = []
+                    for (var j = 0; j < powers.length; j++) {
+                        // TODO: replace 0 with the correct point
+                        pieData.push(powers[j].data[point.index]);
+                    }
+                    console.log(pieData)
+                    plotPie(pieData);
+
+                }                
+                
             }
         }
     );
@@ -69,24 +91,70 @@ function syncExtremes(e) {
     }
 }
 
+var powers = []
+
 // Get the data. The contents of the data file can be viewed at
 Highcharts.ajax({
-    url: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/activity.json',
+    url: 'https://raw.githubusercontent.com/sharmimathur/dsc106-hw3/master/assets/springfield.json',
     //'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/activity.json',
     //'https://raw.githubusercontent.com/sharmimathur/dsc106-hw3/master/assets/sample_data.json',
+    //'https://raw.githubusercontent.com/sharmimathur/dsc106-hw3/master/assets/springfield.json',
     dataType: 'text',
     success: function (activity) {
 
-        console.log('initialize');
-        activity = JSON.parse(activity);
+        console.log('parse');
+        var activity = JSON.parse(activity);
         console.log(activity);
 
-        console.log(activity.id);
-        console.log(activity.datasets);
-        //TODO: try for loop to see how to access the data
-        
-        activity.datasets.forEach(function (dataset, i) {
+        activity.forEach(function (dataset, i) {
+            console.log(dataset.type);
+            if (dataset.type == 'power') {
+                powers.push(dataset);
+            }
+        });
 
+        var times = [];
+        for (var j = powers[0].history.start; j < powers[0].history.last; j+=(5 * 60)) {
+            times.push(new Date(j * 1000));
+        }
+        
+        for (var j = 0; j < powers.length; j++) {
+            var dataset = powers[j];
+            dataset.data = Highcharts.map(dataset.history.data, function (val, j) {
+                return [times[j], val];
+            });
+        }
+          
+
+        var power_ser = [];
+        // change color here, assign each one a color specific to their thing
+        for (var i = powers.length - 1; i >= 0; i--) {
+            power_ser.push({'data': powers[i].data,
+            'name': powers[i].name,
+            'type': 'area',
+            'color': Highcharts.getOptions().colors[i],
+            'fillOpacity': 1,});
+        }
+
+        //TODO: try for loop to see how to access the data
+        var chartDiv = document.createElement('div');
+        chartDiv.className = 'chart';
+        document.getElementById('container').appendChild(chartDiv);
+
+        var pieDiv = document.createElement('div');
+        pieDiv.className = 'chart';
+        document.getElementById('pieGrid').appendChild(pieDiv);
+
+
+        activity.forEach(function (dataset, i) {
+            
+            if (dataset.id == 'Springfield.price') {
+                dataset.step = true;
+            } else {
+                dataset.step = false;
+            }
+
+            if (dataset.type != 'power') {
             // Add X values
             // TODO: change how to get the xvalues
             // something about dataset.data here and below, see what the og code meant and where it got it from
@@ -94,12 +162,17 @@ Highcharts.ajax({
             //     return [dataset.start[j], val];
             // });
 
+            var times = [];
+
+            for (var j = dataset.history.start; j < dataset.history.last; j+=(5 * 60)) {
+                times.push(new Date(j * 1000));
+            }
+
             //so this one returns [x, y]
-            dataset.data = Highcharts.map(dataset.data, function (val, j) {
-                return [activity.xData[j], val];
+            dataset.data = Highcharts.map(dataset.history.data, function (val, j) {
+                return [times[j], val];
             });
 
-            console.log("creation");
             var chartDiv = document.createElement('div');
             chartDiv.className = 'chart';
             document.getElementById('container').appendChild(chartDiv);
@@ -112,7 +185,7 @@ Highcharts.ajax({
                     spacingBottom: 20
                 },
                 title: {
-                    text: dataset.name,
+                    text: dataset.id,
                     align: 'left',
                     margin: 0,
                     x: 30
@@ -130,7 +203,7 @@ Highcharts.ajax({
                         setExtremes: syncExtremes
                     },
                     labels: {
-                        enabled: true
+                        enabled: false
                     }
                 },
                 yAxis: {
@@ -139,11 +212,11 @@ Highcharts.ajax({
                     }
                 },
                 plotOptions: {
-                    series: {
-                        // TODO: CHANGE W NEW DATA
-                        pointStart: Date.UTC(2010, 5, 3),
-                        pointInterval: 24 * 3600 * 1000 // one day
+                    area: {
+                        stacking: 'normal',
+                        fillOpacity: 100
                     }
+                    
                 },
                 tooltip: {
                     positioner: function () {
@@ -161,74 +234,226 @@ Highcharts.ajax({
                     style: {
                         fontSize: '18px'
                     },
-                    valueDecimals: dataset.valueDecimals
+                    valueDecimals: dataset.valueDecimals,
+                    
                 },
                 series: [{
                     // change these values to match sample data, specificlaly start and last
                     data: dataset.data,
-                    name: dataset.id,
-                    type: dataset.type,
-                    color: Highcharts.getOptions().colors[i],
-                    fillOpacity: 0.3,
+                    name: dataset.name,
+                    step: dataset.step,
+                    type: 'line',
+                    color: 'red',
+                    fillOpacity: 1,
                     // TODO: with springfield data, use the units options
                     // tooltip: {
                     //     valueSuffix: ' ' + dataset.units
                     // }
                 }]
             });
+        }
         });
+
+        Highcharts.chart(chartDiv, {
+            chart: {
+                type: 'area',
+                marginLeft: 40, // Keep all charts left aligned
+                spacingTop: 20,
+                spacingBottom: 20
+            },
+            title: {
+                text: dataset.id,
+                align: 'left',
+                margin: 0,
+                x: 30
+            },
+            credits: {
+                enabled: false
+            },
+            xAxis: {
+                type : 'datetime',
+                crosshair: true,
+                events: {
+                    setExtremes: syncExtremes
+                },
+                labels: {
+                    enabled: true
+                }
+            },
+            yAxis: {
+                title: {
+                    text: null
+                }
+            },
+            plotOptions: {
+                area: {
+                    stacking: 'normal',
+                    lineColor: '#666666',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#666666'
+                    },
+                    enableMouseTracking: true
+                }
+            },
+            tooltip: {
+                positioner: function () {
+                    return {
+                        // right aligned
+                        x: this.chart.chartWidth - this.label.width,
+                        y: 10 // align to title
+                    };
+                },
+                formatter: function() {
+                    var tooltipData = [
+                                    {color: '#FF0000'},
+                                    {color: '#008129'},
+                                    {color: '#FFF300'},
+                                    {color: '#0561AD'},
+                                    {color: '#FF9136'},
+                                    {color: '#FF00D1'},
+                                    {color: '#000000'}
+                                ];
+                    //points.length
+                    for (var j = powers.length; j<5;j++){
+                        tooltipData[j].y = this.powers[j].y;
+                    }
+
+                    //this.x
+                    return new Date(5);
+                },
+                borderWidth: 0,
+                backgroundColor: 'none',
+                pointFormat: '{point.y}',
+                headerFormat: '',
+                shadow: false,
+                style: {
+                    fontSize: '18px'
+                },
+                split: true,
+                valueDecimals: dataset.valueDecimals
+            },
+            
+            series: power_ser
+        });
+
+        var pieData = []
+        for (var j = 0; j < powers.length; j++) {
+            // TODO: replace 0 with the correct point
+            pieData.push(powers[j].data[0]);
+        }
+
+        plotPie(pieData);
+
     }
 });
 
 
+function plotPie(dataset) {
+    console.log('u tried');
+    var demand = 0;
+    for (var i = 0; i < dataset.length; i++) {
+        demand += dataset[i][1];
+    }
+    Highcharts.chart('pieGrid', {
+            chart: {
+                renderTo: 'pieGrid',
+                type: 'pie',
+                marginLeft: 40, // Keep all charts left aligned
+                spacingTop: 20,
+                spacingBottom: 20
+            },
+            title: {
+                text: dataset.id,
+                align: 'left',
+                margin: 0,
+                x: 30
+            },
+            credits: {
+                enabled: false
+            },
+            legend: {
+                enabled: true
+            },
+            plotOptions: {
+                pie: {
+                    innerSize: '40%',
+                    cursor: 'pointer',
+                    dataLabels: {
+                        enabled: false,
+                    }   
+                },
+                area: {
+                    lineColor: '#666666',
+                    lineWidth: 1,
+                    marker: {
+                        lineWidth: 1,
+                        lineColor: '#666666'
+                    }
+                },
+                series: {
+                    animation: false
+                }
+            },
+            title: {
+                text: Math.round(demand) + ' MW',
+                align: 'center',
+                verticalAlign: 'middle',
+            },            
+        
+            series: [{
+                data: dataset
+            }]        
+    });
+};
 
 
-
-let sharedConfig = {
-  layout: "1x1",
-  graphset : [
-    {
-      // config for the energy stacked area graph
-      type: 'area',
-      title: {
-        text: 'Generation MW',
-        fontSize: 18,
-      },
-      "crosshair-x":{
-        shared: true
-      },
-      plot: {
-        tooltip:{
-          visible: true
-        },
-        aspect: "spline",
-        stacked: true
-      },
-      plotarea: {
-        margin: "dynamic"
-      },
-      "scale-x": {
-          "min-value": 0,
-          "step": "30minute",
-          "transform": {
-              "type": "date",
-              "all": "%m/%d/%Y<br>%h:%i:%s:%q %A"
-          },
-          "item": {
-              "font-size": 9
-          }
-      },
-      "utc": true,
-      "timezone": 0,
-      'scale-y': {
-          values: "0:80:10",
-          format: "%v",
-          guide: {
-            'line-style': "dotted"
-          }
-        },
-      series: []
-    }    //,
+// let sharedConfig = {
+//   layout: "1x1",
+//   graphset : [
+//     {
+//       // config for the energy stacked area graph
+//       type: 'area',
+//       title: {
+//         text: 'Generation MW',
+//         fontSize: 18,
+//       },
+//       "crosshair-x":{
+//         shared: true
+//       },
+//       plot: {
+//         tooltip:{
+//           visible: true
+//         },
+//         aspect: "spline",
+//         stacked: true
+//       },
+//       plotarea: {
+//         margin: "dynamic"
+//       },
+//       "scale-x": {
+//           "min-value": 0,
+//           "step": "30minute",
+//           "transform": {
+//               "type": "date",
+//               "all": "%m/%d/%Y<br>%h:%i:%s:%q %A"
+//           },
+//           "item": {
+//               "font-size": 9
+//           }
+//       },
+//       "utc": true,
+//       "timezone": 0,
+//       'scale-y': {
+//           values: "0:80:10",
+//           format: "%v",
+//           guide: {
+//             'line-style': "dotted"
+//           }
+//         },
+//       series: []
+//     }    //,
     // {
     //   // config for the price line graph
     //   type: "line",
@@ -307,10 +532,9 @@ let sharedConfig = {
     //   },
     //   series: []
     // }
-  ]
-}
+//   ]
+// }
 
-console.log("hi");
 // let pieConfig = {
 //   type: "pie",
 //   plot: {
